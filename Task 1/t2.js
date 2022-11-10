d316ae9a7807d16d8e4ea7d20fbb2fe36ae08006
R-Remitha <45289832+R-Remitha@users.noreply.github.com>
2022-11-10 23:35:13 +0530

    Update t2.js

--- Task 1/t2.js
+++ Task 1/t2.js
@@ -1,58 +1,79 @@
-function cur()
+function get()
+{
+    tim=setTimeout(get1,3);
+}
+function get1()
+{
+    var x = document.getElementById('rp').value;
+    var res = x.split(/[.।]/);
+    console.log(res);
+    document.getElementById('rp').value="";
+    if(x.indexOf('.')!=-1)
+    {
+        for(i=1;i<res.length-1;i++)
+        {
+            if(i==res.length-2)
             {
-                var curPos =document.getElementById("rp").selectionStart;
-                var x = document.getElementById('rp').value;
-                console.log(curPos);
-                if(x=='')
-                {
-                    document.getElementById("lp").value="No Pasted Text present."
-                }
-                else if(curPos<=42)
-                {
-                    document.getElementById("lp").value='You are on line no: 1';
-                }
-                else
-                {
-                    var v='';
-                    for( var i = 0; i <curPos; i++ )
-                    if( !(x[i] == '\n' || x[i] == '\r') )
-                    {
-                        v += x[i];
-                    }
-                    var res=v.match(/.{1,42}/g) || [];
-                    document.getElementById("lp").value='You are on line no: '+res.length;
-                    
-                }
-                
+                var m=i+')'+res[i]+'.';
             }
-            function get()
+            else
             {
-                tim=setTimeout(get1,3);
+                var m=i+')'+res[i]+'.\n';
             }
-
-            function get1()
-            {
-            var x = document.getElementById('rp').value;
-            var newstr = "";
-            for( var i = 0; i < x.length; i++ )
-            if( !(x[i] == '\n' || x[i] == '\r') )
-            {
-                newstr += x[i];
-            }
-            document.getElementById("rp").value="";
-            var res=newstr.match(/.{1,42}/g) || [];
-            console.log(res);
-            for(var i=0;i<res.length;i++)
-            {
-                document.getElementById("rp").value +=res[i]+'\n';
-            }
-
-            let n=1;
-            document.getElementById("bp").value='';
-            while(n<=res.length)
-            {
-                document.getElementById("bp").value+=n+'\n';
-                n++;
-            }
-
+            document.getElementById('rp').value+=m;
         }
+    }
+    else
+    {
+        for(i=1;i<res.length-1;i++)
+        {
+            if(i==res.length-2)
+            {
+                var m=i+')'+res[i]+'।';
+            }
+            else
+            {
+                var m=i+')'+res[i]+'।\n';
+            }
+            document.getElementById('rp').value+=m;
+        }
+    }
+}
+function cur()
+{
+    var cp=document.getElementById('rp').selectionStart;
+    var y = document.getElementById('rp').value;
+    if(y=='')
+    {
+        document.getElementById('lp').value='\n\n\n\n\n\n\n\nNo pasted text present.';
+    }
+    else
+    {
+        var m=y.split(/\n/);
+        var x = document.getElementById('rp').value;
+        var n='';
+        for(var i=0;i<cp;i++)
+        {
+            n=n+x[i];
+        }
+        var res=n.split(/\n/);
+        var t=res.length;
+        document.getElementById('lp').value='\n\n\n\n\n\n\n\nYou are on line no: '+t;
+        
+    }
+    
+    /*var hi=m[t-1];
+    for(var t=0;t<=m.length;t++)
+    {
+        if(m[t]==hi)
+        {
+            var y=m[t].replace(m[t], '<mark>$&</mark>');
+            document.getElementById('rp').value+=y;
+        }
+        else
+        {
+            document.getElementById('rp').value+=m[t];
+        }
+    }*/
+}
+
